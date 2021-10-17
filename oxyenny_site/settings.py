185b8d941afa7714213e81951f19fc4f85aa08b9
Pashs-ba <pashs.ba@gmail.com>
2021-10-17 16:52:55 +0300

    some improvements

--- oxyenny_site/settings.py
+++ oxyenny_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q71y-x=+!l4(4v#e-)hg-x=ft(ks38=3&a70xj(pe6q(%*b*$#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,7 +127,7 @@ STATIC_DIRS = [
     "main/static",
     "products/static"
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
